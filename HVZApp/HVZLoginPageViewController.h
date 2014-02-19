9f5e09eb3afe5bf6064a41437c4c375ff7f7afe6
JennerFelton <jfelton@g.hmc.edu>
2014-02-18 17:31:49 -0800

    New Login Page

---
+++ HVZApp/HVZLoginPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  HVZLoginPageViewController.h
+//  HVZApp
+//
+//  Created by jarthurcs on 2/18/14.
+//  Copyright (c) 2014 LSD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HVZLoginPageViewController : UIViewController
+
+@end
---
+++ HVZApp/HVZLoginPageViewController.m
@@ -0,0 +1,38 @@
+//
+//  HVZLoginPageViewController.m
+//  HVZApp
+//
+//  Created by jarthurcs on 2/18/14.
+//  Copyright (c) 2014 LSD. All rights reserved.
+//
+
+#import "HVZLoginPageViewController.h"
+
+@interface HVZLoginPageViewController ()
+
+@end
+
+@implementation HVZLoginPageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
