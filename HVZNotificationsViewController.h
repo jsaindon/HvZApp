08afb844588543687be814978b53003ce4eb5182
JennerF <jfelton@g.hmc.edu>
2014-04-23 00:59:48 -0700

    added messaging

---
+++ HVZNotificationsViewController.h
@@ -0,0 +1,13 @@
+//
+//  HVZNotificationsViewController.h
+//  HVZApp
+//
+//  Created by CS121 on 4/20/14.
+//  Copyright (c) 2014 LSD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HVZNotificationsViewController : UIViewController
+
+@end
